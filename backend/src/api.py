5c086cc7d1d72f9b30038e172c7e5fd3555025a2
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2020-11-07 14:38:55 +0200

    Implement error handler for AuthError

--- backend/src/api.py
+++ backend/src/api.py
@@ -119,7 +119,10 @@ def unprocessable(error):
 '''
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+                    "success": False, 
+                    "error": error.status_code,
+                    "message": error.error
+                    }), error.status_code
